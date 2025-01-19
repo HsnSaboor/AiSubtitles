7addf3f149981e1834964e19fb5ab4da71fb4c80
Saboor Hassan <156575127+HsnSaboor@users.noreply.github.com>
2025-01-19 22:10:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ import tiktoken
 import os
 from openai import OpenAI
 
-# Set up API key and base URL
-API_KEY = "Free-For-YT-Subscribers-@DevsDoCode-WatchFullVideo"
-BASE_URL = "https://api.ddc.xiolabs.xyz/v1"
-
 token = os.environ["GITHUB_TOKEN"]
 endpoint = "https://models.inference.ai.azure.com"
 model_name = "gpt-4o"
@@ -199,7 +195,7 @@ def translate_json_chunk(json_chunk, system_prompt, model="gpt-4o"):
     try:
         input_json = json.dumps(json_chunk)
         response = client.chat.completions.create(
-            model="provider-4/gpt-4o",
+            model="gpt-4o",
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": f"The JSON object you will be translating is: {input_json}"}
@@ -327,7 +323,7 @@ def translate_text(text, request_no):
     try:
         st.write(f"Request {request_no}: Translating line: {text}")
         response = client.chat.completions.create(
-            model="provider-4/gpt-4o",
+            model="gpt-4o",
             messages=[
                 {"role": "user", "content": f"Translate this Turkish text to Urdu: {text}"}
             ]
